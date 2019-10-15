aba126f9ef3fba8f5a04c524a1d808cff96936b0
Brian Spray <brian.spray93@gmail.com>
2019-10-15 15:52:09 -0400

    Done with day 1

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,17 @@
 
 import './styles.css';
-console.log('Ready to Party With Some TypeScript!');
+import { multiply } from './math';
 
+const number1El = document.getElementById('number1') as HTMLInputElement;
+const number2El = document.getElementById('number2') as HTMLInputElement;
+const multiplyButton = document.getElementById('multiply') as HTMLInputElement;
+const answerEl = document.getElementById('answer') as HTMLSpanElement;
+
+multiplyButton.addEventListener('click', function () {
+    const n1 = number1El.valueAsNumber;
+    const n2 = number2El.valueAsNumber;
+
+    const answer = multiply(n1, n2).toString();
+
+    answerEl.innerHTML = answer;
+});
